ed9c3134a40c7cb8df0c91a84a0c928fa3e8166f
Marcia Gabrielle Apolinario <gabriellemga@gmail.com>
2025-01-07 20:45:00 -0300

    Mudaça

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 20;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeAleatorio();
 let tentativas = 1;
 
@@ -10,8 +10,8 @@ function inserirTexto(tag, titulo){
 }
 
 function exibirMensagemInicial(){
-    inserirTexto ('h1', ' Jogo do número secreto');
-    inserirTexto ('p', 'Escolha um numero entre 1 e 20');
+    inserirTexto ('h1', '  número secreto');
+    inserirTexto ('p', 'Escolha um numero entre 1 e 10');
 }
 
 exibirMensagemInicial();
@@ -35,7 +35,7 @@ function verificarChute(){
 }
 
 function gerarNumeAleatorio(){
-        let numeroEscolhido = parseInt(Math.random() *numeroLimite + 2);
+        let numeroEscolhido = parseInt(Math.random() *numeroLimite + 1);
         let quantidadeElementosLista = listaDeNumerosSorteados.length;
 
     if (quantidadeElementosLista == numeroLimite){
